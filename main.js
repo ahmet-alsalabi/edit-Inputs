169b1bc7f048d59e5c18c72fa7d8f8d05f567074
ahmet-alsalabi <ahmetalsalabi@gmail.com>
2024-10-03 23:36:01 +0300

    finish the project

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function capitall() {
+    let val = document.getElementById("custName").value;
+    let amplify = val.toUpperCase();
+    document.getElementById("cap").textContent = amplify;    
+
+}
+
+
+function small() {
+    let val = document.getElementById("custName").value;
+    let diminution = val.toLowerCase();
+    document.getElementById("sma").textContent = diminution;    
+
+}
+
