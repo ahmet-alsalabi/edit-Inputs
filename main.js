7b3762b988c0b08a5412218ed869e06b609c2ff6
ahmet-alsalabi <ahmetalsalabi@gmail.com>
2024-10-05 15:10:03 +0300

    switch the function to arrow function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function capitall() {
+const capitall = () => {
     let val = document.getElementById("custName").value;
     let amplify = val.toUpperCase();
     document.getElementById("cap").textContent = amplify;    
@@ -7,7 +7,7 @@ function capitall() {
 }
 
 
-function small() {
+const small = () => {
     let val = document.getElementById("custName").value;
     let diminution = val.toLowerCase();
     document.getElementById("sma").textContent = diminution;    
