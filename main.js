f7a9e8c790df8b53d825770fe875b41765648c94
ahmet-alsalabi <ahmetalsalabi@gmail.com>
2024-10-22 23:20:17 +0300

    edit the result

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-
 const capitall = () => {
     let val = document.getElementById("custName").value;
     let amplify = val.toUpperCase();
-    document.getElementById("cap").textContent = amplify;    
-
+    document.getElementById("output").textContent = amplify;    
 }
-
 
 const small = () => {
     let val = document.getElementById("custName").value;
     let diminution = val.toLowerCase();
-    document.getElementById("sma").textContent = diminution;    
-
+    document.getElementById("output").textContent = diminution;    
 }
-
